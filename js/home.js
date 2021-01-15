a0101679a4e97d57bc11e1051174ca09ab0a0b58
Alejandro <alejandrosantalucia@gmail.com>
2021-01-15 15:32:35 -0300

    primer commit

---
+++ js/home.js
@@ -0,0 +1,139 @@
+console.log('hola mundo!');
+const noCambia = "Alejandro";
+
+let cambia = "@ASL"
+
+function cambiarNombre(nuevoNombre) {
+  cambia = nuevoNombre
+}
+
+
+
+const getUser1 = new Promise ( function (todoBien, todoMal) // la funcion de la promesa esta compuesta por dos parametros, uno por si el resultado de la promesa es positivo y el otro por si el resultado es negativo
+{
+  setTimeout ( function(){todoBien("primer mensaje de prueba")}, 
+  3000)
+}
+)
+const getUser2 = new Promise ( function (todoBien, todoMal) // la funcion de la promesa esta compuesta por dos parametros, uno por si el resultado de la promesa es positivo y el otro por si el resultado es negativo
+{
+  setTimeout ( function(){todoBien("segundo mensaje de prueba")}, 
+  5000)
+}
+)
+
+// getUser
+//  .then (function () {
+//    console.log ("aguante todo")
+//  })
+//  .catch (function (MensajeDeTodoMAl) {
+//    console.log (MensajeDeTodoMAl)
+//  })
+
+ Promise.race ([
+   getUser1,
+   getUser2
+ ])
+ .then (function (MensajeDePrueba) {
+  console.log (MensajeDePrueba)
+})  
+.catch (function (MensajeDePrueba) {
+  console.log (MensajeDePrueba)
+})
+
+
+
+// -------------------- ajax para jQuery
+ 
+
+$.ajax('https://randomuser.me/api/', {
+    method: 'GET',
+    success: function(data){
+        console.log(data)
+    },
+    error: function(error){
+        console.log(error)
+    }
+
+})
+
+fetch ('https://randomuser.me/api/')
+  .then (function (response) {
+    // console.log (response)
+    return response.json()
+  })
+  .then (function (user) {
+    console.log ('user', user.results[0].name.first)
+  })
+  .catch (function () {
+    console.log ("algo fallo")
+  });
+
+
+
+
+(async function load () {
+  async function getData (url) {
+    const response = await fetch  (url)
+    const data = await response.json()
+    return data
+  }
+
+  const actionList = await getData ('https://yts.mx/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData ('https://yts.mx/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData ('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  console.log (actionList, dramaList , animationList)
+
+function videoItemTemplate ( movie) {
+  return (  
+    `<div class="primaryPlaylistItem">
+    <div class="primaryPlaylistItem-image">
+     <img src=${movie.medium_cover_image}>
+    </div>
+    <h4 class="primaryPlaylistItem-title">
+      ${movie.title}
+    </h4>
+    </div>`
+  )}
+
+
+  const $actionContainer = document.getElementById('action')
+  actionList.data.movies.forEach((movie) => {
+
+    const HTMLString = videoItemTemplate (movie)
+    const html = document.implementation.createHTMLDocument()
+    html.body.innerHTML =  HTMLString
+    // debugger  // debugger para chequear en consola si lo creo correctamente.
+    
+    
+    $actionContainer.append(html.body.children[0])
+    // console.log(HTMLString)
+    
+  });
+
+
+
+
+
+const $dramaContainer = document.getElementById('drama')
+const $animationContainer = document.getElementById('animation')
+
+const $featuringContainer = document.getElementById('featuring')
+const $form = document.getElementById('form')
+const $home = document.getElementById('home')
+
+const $modal = document.getElementById('modal')
+const $overlay = document.getElementById('overlay')
+const $hideModal = document.getElementById('hide-modal')
+
+const $modalTitle = $modal.querySelector('h1')
+const $modalImage = $modal.querySelector('img')
+const $modalDescription = $modal.querySelector('p')
+
+
+
+
+  // console.log (videoItemTemplate ('images/covers/bitcoin.jpg', 'bitcoin'))
+
+
+}) ();
